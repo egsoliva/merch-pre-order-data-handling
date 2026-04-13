c9f42c2f90a0fd87c9cc99d22d8b14b3ed1f35bd
egsoliva <egsoliva06@gmail.com>
2026-04-14 01:17:22 +0800

    added shirts class

---
+++ shirts.py
@@ -0,0 +1,39 @@
+class CanWeDoBetterEarthyGreen:
+    def __init__(self):
+        self._sizes = {"XS": 0, "S": 0, "M": 0, "L": 0, "XL": 0}
+
+    @property
+    def sizes(self):
+        return self._sizes
+
+class CanWeDoBetterTangyOrange:
+    def __init__(self):
+        self._sizes = {"XS": 0, "S": 0, "M": 0, "L": 0, "XL": 0}
+    
+    @property
+    def sizes(self):
+        return self._sizes
+
+class CanWeDoBetterCrimsonRed:
+    def __init__(self):
+        self._sizes = {"XS": 0, "S": 0, "M": 0, "L": 0, "XL": 0}
+
+    @property
+    def sizes(self):
+        return self._sizes
+
+class BruteForceLight:
+    def __init__(self):
+        self._sizes = {"XS": 0, "S": 0, "M": 0, "L": 0, "XL": 0}
+
+    @property
+    def sizes(self):
+        return self._sizes
+
+class BruteForceDark:
+    def __init__(self):
+        self._sizes = {"XS": 0, "S": 0, "M": 0, "L": 0, "XL": 0}
+
+    @property
+    def sizes(self):
+        return self._sizes
