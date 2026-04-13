2671705ebf7c97aaaf50da37468f181f5fb62053
egsoliva <egsoliva06@gmail.com>
2026-04-14 01:17:48 +0800

    added strenum files

---
+++ lanyards.py
@@ -0,0 +1,9 @@
+from enum import StrEnum
+
+class Lanyards(StrEnum):
+    WC = "White Cream"
+    DT = "Dark Tendrils"
+    DRF = "Dark Red Fiery"
+    RF = "Red Fiery"
+    DTWC = "Dark Tendrils/White Cream"
+    DRFRF = "Dark Red Fiery/Red Fiery"
